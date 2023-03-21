b006f6b41013f94749c50cf2cea77e5e564a46b2
Matias Cejas <matiascejas8@gmail.com>
2023-03-21 15:38:57 -0300

    feat: hide sign out button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 type HeaderProps = {
@@ -6,17 +7,20 @@ type HeaderProps = {
 };
 
 const Header: React.FC<HeaderProps> = ({ onSignOut }) => {
+  const auth = useSelector((state: any) => state.auth);
   return (
     <header className="bg-sky-500 p-4 flex justify-between items-center">
       <h1 className="text-white text-xl font-bold">
         <Link to={"/"}>Calculator App</Link>
       </h1>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={onSignOut}
-      >
-        Sign Out
-      </button>
+      {auth?.user && (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          onClick={onSignOut}
+        >
+          Sign Out
+        </button>
+      )}
     </header>
   );
 };
